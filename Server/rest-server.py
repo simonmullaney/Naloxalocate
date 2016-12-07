bd076ab6f3c60983ea06c92daf2b2ce009d2a615
psdcon <psdcon@gmail.com>
2016-12-07 20:49:30 +0000

    Added function stubs to server. Added database

--- Server/rest-server.py
+++ Server/rest-server.py
@@ -1,6 +1,38 @@
-from flask import Flask
-app = Flask(__name__)
+#!flask/bin/python
+from __future__ import print_function # In python 2.7
+import sqlite3
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# DB Structure
+# id (device id)
+# location
+# update time
+
+dbPath = "database.sqlite"
+
+db = sqlite3.connect(dbPath)
+db.row_factory = sqlite3.Row
+
+
+@app.route('/naloxalocate/api/v1.0/users', methods=['GET'])
+def get_users():
+    pass
+
+@app.route('/naloxalocate/api/v1.0/users/<string:user_id>', methods=['GET'])
+def get_user(user_id):
+    pass
+
+@app.route('/naloxalocate/api/v1.0/users', methods=['POST'])
+def create_user():
+    pass
+
+@app.route('/naloxalocate/api/v1.0/users/<string:user_id>', methods=['PUT'])
+def update_day(user_id):
+    pass
+
+@app.route('/naloxalocate/api/v1.0/users/<string:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
