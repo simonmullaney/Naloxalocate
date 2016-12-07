56c63eeea1814d23b511a9f61df82012f2df74d3
psdcon <psdcon@gmail.com>
2016-12-07 21:14:58 +0000

    GET request returning users as json

--- Server/rest-server.py
+++ Server/rest-server.py
@@ -2,20 +2,55 @@
 from __future__ import print_function # In python 2.7
 import sqlite3
 
+from flask import Flask, jsonify, abort, request, make_response, g
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__, static_url_path="")
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+
+
+###############################################################################
+#
+# Database
+#
+###############################################################################
+
 # DB Structure
-# id (device id)
-# location
-# update time
+# Users table
+#   id (device id)
+#   location
+#   update time
 
-dbPath = "database.sqlite"
+DATABASE = 'database.sqlite'
 
-db = sqlite3.connect(dbPath)
-db.row_factory = sqlite3.Row
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+###############################################################################
+#
+# API Calls
+#
+###############################################################################
 
 @app.route('/naloxalocate/api/v1.0/users', methods=['GET'])
 def get_users():
-    pass
+    # Get db connection and select users from db
+    cur = get_db().cursor()
+    cur.execute('SELECT * FROM users')
+    # Fetch all users from db result
+    users = cur.fetchall()
+    return jsonify(users=users)
+
 
 @app.route('/naloxalocate/api/v1.0/users/<string:user_id>', methods=['GET'])
 def get_user(user_id):
