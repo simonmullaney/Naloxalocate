2d013d51ae854116339f9133e60854f88981f825
12362146 <simon.mullaney@ucdconnect.ie>
2016-12-09 19:24:45 +0000

    creating main layout

---
+++ Android/app/src/main/java/com/apaulling/naloxalocate/TimerBeginActivity.java
@@ -0,0 +1,40 @@
+package com.apaulling.naloxalocate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by simonmullaney on 09/12/2016.
+ */
+
+public class TimerBeginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer_begin);
+
+
+
+
+        /*
+        Button btnBeginTimerActivity = (Button) findViewById(R.id.btnBeginTimerActivity);
+
+
+        btnBeginTimerActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimerBeginActivity.this.startActivity(new Intent(TimerBeginActivity.this, TimerActivity.class));
+            }
+        });
+        */
+    }
+}
+
+
+
+
+
