7951aab257f2c73e1ed3323b07325a52f3893035
12362146 <simon.mullaney@ucdconnect.ie>
2016-12-10 14:49:29 +0000

    Text fields in timer setup page.

--- Android/app/src/main/java/com/apaulling/naloxalocate/TimerActivity.java
+++ Android/app/src/main/java/com/apaulling/naloxalocate/TimerActivity.java
@@ -1,10 +1,19 @@
 package com.apaulling.naloxalocate;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static com.apaulling.naloxalocate.R.id.checkBox;
 
 /**
  * Created by psdco on 08/12/2016.
@@ -29,6 +38,46 @@ public class TimerActivity extends AppCompatActivity {
 
             }
         });
+
+
+        EditText Contact_number = (EditText) findViewById(R.id.Contact_number);
+
+        Contact_number.setOnEditorActionListener(new TextView.OnEditorActionListener(){
+            @Override
+            public boolean onEditorAction (TextView textview,int i, KeyEvent keyEvent){
+                boolean handled = false;
+                if (i == EditorInfo.IME_ACTION_NEXT) {
+                    String inputText = textview.getText().toString();
+                    Toast.makeText(TimerActivity.this, "" + inputText, Toast.LENGTH_SHORT).show();
+
+                }
+                return handled;
+            }
+        });
+
+        /*
+        //code to save inserted details
+        ch = (CheckBox)findViewById(R.id.checkBox);
+
+        ch.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if(ch.isChecked()){
+
+                    SharedPreferences settings = getSharedPreferences(PREFRENCES_NAME, 0);
+                    settings.edit().putBoolean("check",true).commit();
+
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "Uncheck", Toast.LENGTH_SHORT).show();
+                }}
+        });
+
+    }
+    */
 
     }
 
