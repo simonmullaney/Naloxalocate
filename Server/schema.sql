5177d77cd86c4438a105ad46a7d872b9fbf0bbe7
psdcon <psdcon@gmail.com>
2016-12-11 17:23:48 +0000

    Server PUT updating and GET nearby users working

--- Server/schema.sql
+++ Server/schema.sql
@@ -1,6 +1,8 @@
 drop table if exists users;
-CREATE TABLE `users` (
-  `id`  INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-  `location`  TEXT,
-  `last_updated`  INTEGER
+CREATE TABLE "users" (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+  `latitude` REAL,
+  `longitude` REAL,
+  `accuracy` REAL,
+  `last_updated` INTEGER
 )
